c3996196f48f2f3af1fa8f078bdf126f16fa7b45
CakeLancelot <CakeLancelot@users.noreply.github.com>
2024-04-22 15:27:35 -0500

    WIP

--- ATBPServer/static/js/atbp-client.js
+++ ATBPServer/static/js/atbp-client.js
@@ -1,21 +1,88 @@
 var unity;
-window.onload = function () {
-  unity = document.getElementById('unity_player');
-};
 
-function OnResize() {
-  unity.style.width = window.innerWidth + 'px';
-  unity.style.height = window.innerHeight + 'px';
+function isInternetExplorer() {
+  if (window.document.documentMode) {
+    return true;
+  }
+  return false;
 }
 
+function createParam(name, val) {
+  var param = document.createElement('param');
+  param.setAttribute('name', name)
+  param.setAttribute('value', val)
+  return param
+}
+
+function embedUnity() {
+  var object = document.createElement('object');
+  object.setAttribute('class', "embed-responsive embed-responsive-16by9");
+  object.setAttribute('classid', "clsid:444785F1-DE89-4295-863A-D46C3A781394");
+  object.setAttribute('codebase', "undefined/UnityWebPlayer.cab#version=2,0,0,0");
+  object.setAttribute('id', "unity-object");
+  object.setAttribute('width', "100%");
+  object.setAttribute('height', "100%");
+
+  var params = {
+    'src': "CNChampions.unity3d",
+    'bordercolor': "DF2900",
+    'backgroundcolor': "021E2F",
+    'textcolor': "DF2900",
+    'disableContextMenu': "true",
+    'disablefullscreen': "false",
+    'logoimage': "logoimage.png",
+    'progressbarimage': "progressbarimage.png",
+    'progressframeimage': "progressframeimage.png"
+  }
+
+  if(!isInternetExplorer()) {
+    var embed = document.createElement('embed');
+    embed.setAttribute('class', "embed-responsive-item");
+    embed.setAttribute('type', "application/vnd.unity");
+    embed.setAttribute('id', "unity-embed");
+    Object.keys(params).forEach(function(key) {
+      embed.setAttribute(key, params[key]);
+    });
+  } else {
+    Object.keys(params).forEach(function(key) {
+      var paramToAppend = createParam(key, params[key])
+      object.appendChild(paramToAppend);
+    });
+  }
+
+  var div = document.getElementById("embed-container");
+  div.innerHTML = '';
+  if(!isInternetExplorer()) {object.appendChild(embed)};
+  div.appendChild(object);
+  unity = document.getElementById('unity-object');
+}
+
+window.onload = function () {
+  if(!isInternetExplorer()) {
+    for (var i = 0;i < navigator.plugins.length;i++) {
+      if (navigator.plugins[i].name.indexOf("Unity Player") != -1) {
+        embedUnity();
+      }
+    }
+  } else {
+    try {
+      var plugin = new ActiveXObject('UnityWebPlayer.UnityWebPlayer.1');
+      embedUnity();
+    } catch (e) {
+      console.log("Failed to embed ActiveXObject")
+    }
+  }
+};
+
 function Fireteam_CheckMSIBLoggedIn(name, callback) {
+  /*
   returnOK = document.cookie
     .split('; ')
     .find((row) => row.startsWith('logged'))
     ?.split('=')[1];
   console.log(returnOK);
   if (returnOK == undefined) returnOK = 'false';
-  unity.SendMessage(name, callback, returnOK);
+  unity.SendMessage(name, callback, returnOK);*/
 }
 
 function Fireteam_GetCookies(name, callback) {
@@ -68,7 +135,7 @@ var UnityRequest = function (
 var LoginModule = function () {};
 
 LoginModule.showLoginWindow = function () {
-  if (true) {
+/*  if (true) {
     //TODO: Fix so it is variable
     window.location.href =
       'https://discord.com/api/oauth2/authorize?client_id=' +
@@ -129,5 +196,5 @@ LoginModule.showLoginWindow = function () {
         console.log(e);
       }
     }
-  }
+  }*/
 };
